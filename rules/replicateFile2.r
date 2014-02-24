a25038fca4efba1a52c89aae423aca405cd7e9cf
lphan <lphan@68e52488-0a15-44bc-a314-416658652264>
2014-02-24 10:04:22 +0000

    change some layout

--- rules/replicateFile2.r
+++ rules/replicateFile2.r
@@ -1,15 +1,9 @@
 #
-# ---- README ----
 # Rule for replication single DataObj at Input from 1.DataCenter to 2.DataCenter
 # Goal: transfer single DataObj from Path Source_A to Target_B without creating new PID at Source_A. 
-# .... One control-file will be created (named file.statistic) to control stand of transfer process ....
-# .... Be sure to set User with OWN-right on the control-file, better on collection where control-file will be saved ....
-# .... Do not forget to setup user rodsadmin to transfer BIG-DATA or DATA with BIG-Size .... 
-# .... Do not forget to change PATH at 'INPUT (line 73)' and 'writeFile for control-file (line 71)' .....
-# .... Using this Rule when data already has its PID ....
-# .... HINT: Do some tests before applying on real-data ....
-# .... My Email: l.phan@fz-juelich.de ....
-# 
+#
+# Author: Long Phan, Juelich
+#  
 replicate {
 	# Transfer Status, default = 0
 	*status = "";
@@ -42,18 +36,19 @@ replicate {
 		foreach   ( *BS )    {
 		        msiGetValByKey(*BS,"DATA_CHECKSUM", *checksum0);
 		        logInfo("checksum0 = *checksum0");
-			msiGetValByKey(*BS,"DATA_SIZE", *sizefile);
+				msiGetValByKey(*BS,"DATA_SIZE", *sizefile);
 		    }
 
 		*checksum1 = "";
 		msiSplitPath(*replicaPATH,*parentD,*childD);
-		msiExecStrCondQuery("SELECT DATA_CHECKSUM WHERE COLL_NAME = '*parentD' AND DATA_NAME = '*childD'" ,*BD);
+		msiExecStrCondQuery("SELECT DATA_CHECKSUM, DATA_SIZE WHERE COLL_NAME = '*parentD' AND DATA_NAME = '*childD'" ,*BD);
 		foreach   ( *BD )    {
 		        msiGetValByKey(*BD,"DATA_CHECKSUM", *checksum1);
 		        logInfo("checksum1 = *checksum1");
+		        msigetValByKey(*BD,"DATA_SIZE", *sizefile2);
 		    }
 
-		if (*checksum0 != *checksum1) {
+		if(*checksum0 != *checksum1 && *sizefile != *sizefile2) {
 		        searchPID(*PATH, *replicaPATH);
 		        logInfo("*checksum0 != *checksum1, existing_pid = *pid");
 		        logInfo("replication from *PATH to *replicaPATH");
@@ -68,7 +63,7 @@ replicate {
 	}
 
 	# also write log file to report stand even if everything went successful
-	writeFile("/DATACENTER/standlog/file.statistic",*status);
+	writeFile("/DATACENTER/standlog/file.stat",*status);
 }
 INPUT *PATH="/DATACENTER/PHANreplica/Coll17/csc10.ta",*replicaPATH="/DATACENTER2/JUELICHreplica/Coll17/csc10.ta"
 OUTPUT ruleExecOut
