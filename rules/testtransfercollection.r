6f0813ea1c05f0e5b7c7419933e168de2553edcc
ccacciari <ccacciari@68e52488-0a15-44bc-a314-416658652264>
2014-05-27 13:13:28 +0000

    modified testtransfercollection.r to execute it in delayed mode

--- rules/testtransfercollection.r
+++ rules/testtransfercollection.r
@@ -1,11 +1,15 @@
 #
 # Test for transfer complete Collection Dir and all of data objects inside it from *Path to *replicaPath. 
+# It use the delay mode to try again even in case of failure.
+# It uses the EUDAT rule: EUDATTransferCollection(*path_of_transfered_coll,*target_of_transfered_coll,
+#                                                 *incremental,*recursive)
 #
 test {
-
-     EUDATTransferCollection(*Path,*replicaPath,bool("false"),bool("true"));
+    delay("<EF>1s REPEAT UNTIL SUCCESS OR 10 TIMES</EF>") {
+        EUDATTransferCollection(*Path,*replicaPath,bool("true"),bool("true"));
+    }
 }
 
-INPUT *Path="/CINECA/home/testuser/testPID/10x10nested.dir",*replicaPath="/CINECA2/home/testuser#CINECA/testData"
+INPUT *Path="/CINECA/home/testuser/testPID/1000x3nested.dir",*replicaPath="/CINECA2/home/testuser#CINECA/testData"
 OUTPUT ruleExecOut
 ~                 
