58105381d91b4965989830ee949e4bcf706fb927
lphan <lphan@68e52488-0a15-44bc-a314-416658652264>
2014-06-11 11:00:06 +0000

    Rule to test DoReplication with/ without msiCollCreate

---
+++ rules/testDoReplication.r
@@ -0,0 +1,26 @@
+#
+# Test Do Replication
+#
+# 1.Test: 
+# Source: /DATACENTER2/DATA/xaaaaaaaadv, Destination: /DATACENTER2/DATA/Test/xaaaaaaaadv
+# (with msiCollCreate)
+# Result: DONE, OK.
+#
+# 2.Test:
+# without msiCollCreate
+# Source: /DATACENTER2/DATA/xaaaaaaaadv, Destination: /DATACENTER2/DATA/Test2/xaaaaaaaadv
+# Result: DONE, OK.
+#
+test {
+
+	msiSplitPath(*destination, *parent, *child);
+	
+	# Test with/ without this microservice
+    msiCollCreate(*parent, "1", *collCreateStatus);
+
+    #rsync object (make sure to supply "null" if dest resource should be the default one) 
+    msiDataObjRsync(*source, "IRODS_TO_IRODS", "null", *destination, *rsyncStatus);
+
+}
+INPUT  *source = "/DATACENTER2/DATA/xaaaaaaaadv", *destination = "/DATACENTER2/DATA/Test/xaaaaaaaadv"
+OUTPUT ruleExecOut
