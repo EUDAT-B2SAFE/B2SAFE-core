89ca9b487d3a53d022c407358258d1f1e68bf8f2
Robert Verkerk <robertv@irodstest2.storage.surfsara.nl>
2019-03-08 12:32:49 +0100

    Implement conversion of install.conf to install.json and implement install.py instead of install.sh

--- packaging/create_deb_package.sh
+++ packaging/create_deb_package.sh
@@ -96,60 +96,37 @@ cat > $RPM_BUILD_ROOT${PACKAGE}/DEBIAN/postinst << EOF
 # script for postinstall actions
 
 # create configuration file if it does not exist yet
-INSTALL_CONF=${IRODS_PACKAGE_DIR}/packaging/install.conf
+INSTALL_CONF=${IRODS_PACKAGE_DIR}/packaging/install.json
 
 if [ ! -e \$INSTALL_CONF ]
 then
 
 cat > \$INSTALL_CONF << EOF2
-#
-# parameters for installation of irods module B2SAFE
-#
-# the absolute directory where the irods config is installed
-IRODS_CONF_DIR=/etc/irods
-#
-# the absolute directory where irods is installed
-IRODS_DIR=/var/lib/irods
-#
-# the directory where B2SAFE is installed as a package
-B2SAFE_PACKAGE_DIR=${IRODS_PACKAGE_DIR}
-#
-# the default iRODS resource to use. Will be set in core.re
-DEFAULT_RESOURCE=demoResc
-#
-# epic credentials type and location
-CRED_STORE_TYPE=os
-CRED_FILE_PATH=\\\$B2SAFE_PACKAGE_DIR/conf/credentials
-SERVER_ID="irods://<fully_qualified_hostname>:1247"
-#
-# epic credentials file usage parameters
-#
-# epicclient2 parameters
-HANDLE_SERVER_URL=<https://epic3.storage.surfsara.nl:8001>
-PRIVATE_KEY=</path/prefix_suffix_index_privkey.pem>
-CERTIFICATE_ONLY=</path/prefix_suffix_index_certificate_only.pem>
-PREFIX=<ZZZ>
-HANDLEOWNER="200:0.NA/\\\$PREFIX"
-REVERSELOOKUP_USERNAME=<ZZZ>
-HTTPS_VERIFY="True"
-#
-# users for msiexec command
-USERS="user0#Zone0 user1#Zone1"
-#
-# loglevel and log directory
-# possible log levels: DEBUG, INFO, WARNING, ERROR, CRITICAL
-LOG_LEVEL=DEBUG
-LOG_DIR=/var/log/irods
-#
-#
-# iRODS behavioral parameters
-#
-# check if user is authorized to perform several functions
-AUTHZ_ENABLED=true
-#
-# check if message queue is enabled
-MSG_QUEUE_ENABLED=false
-#
+{
+  "b2safe_package_dir": "${IRODS_PACKAGE_DIR}",
+  "irods_conf_dir": "/etc/irods",
+  "irods_dir": "/var/lib/irods/iRODS",
+  "irods_default_resource": "demoResc",
+  "cred_store_type": "os",
+  "cred_file_path": "${IRODS_PACKAGE_DIR}/conf/credentials",
+  "server_id": "irods://<fully_qualified_hostname>:1247",
+  "server_api_reg": "irods://<fully_qualified_hostname>:1247",
+  "server_api_pub": "irods://<fully_qualified_hostname>:1247",
+  "handle_server_url": "https://epic6.storage.surfsara.nl:8003",
+  "handle_private_key": "</path/prefix_suffix_index_privkey.pem>",
+  "handle_certificate_only": "</path/prefix_suffix_index_certificate_only.pem>",
+  "handle_prefix": "<ZZZ>",
+  "handle_owner": "200:0.NA/<ZZZ>",
+  "handle_reverse_lookup_name": "<ZZZ>",
+  "handle_reverse_lookup_password": "<reverse_lookup_password>",
+  "handle_https_verify": True,
+  "handle_users": [ "user0#Zone0", "user1#Zone1" ],
+  "log_level": "INFO",
+  "log_directory": "/var/log/irods",
+  "shared_space": "",
+  "authz_enabled": true,
+  "msg_queue_enabled": false
+}
 EOF2
 
 fi
@@ -162,11 +139,11 @@ The package b2safe has been installed in ${IRODS_PACKAGE_DIR}.
 To install/configure it in iRODS do following as the user who runs iRODS :
 
 # update install.conf with correct parameters with your favorite editor
-sudo vi ${IRODS_PACKAGE_DIR}/packaging/install.conf
+sudo vi ${IRODS_PACKAGE_DIR}/packaging/install.json
 
 # install/configure it as the user who runs iRODS
 source /etc/irods/service_account.config 
-sudo su - \\\$IRODS_SERVICE_ACCOUNT_NAME -s "/bin/bash" -c "cd ${IRODS_PACKAGE_DIR}/packaging/ ; ./install.sh"
+sudo su - \\\$IRODS_SERVICE_ACCOUNT_NAME -s "/bin/bash" -c "cd ${IRODS_PACKAGE_DIR}/packaging/ ; ./install.py"
 
 EOF1
 
